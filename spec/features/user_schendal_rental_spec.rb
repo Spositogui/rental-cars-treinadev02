82ddbfba1d45588363badb9c2738298fc223f356
guilherme <developersposito@gmail.com>
2019-12-11 01:30:39 -0300

    User schendal rental

--- spec/features/user_schendal_rental_spec.rb
+++ spec/features/user_schendal_rental_spec.rb
@@ -13,14 +13,14 @@ feature 'user schedule rental' do
     visit root_path
     click_on 'Locações'
     click_on 'Agendar locação'
-    fill_in 'Data de inicio', with: '09/12/2019'
-    fill_in 'Data de término', with: '12/12/2019'
+    fill_in 'Data de inicio', with: 1.day.from_now
+    fill_in 'Data de término', with: 2.days.from_now
     select "#{client.name} - #{client.document}", from: 'Cliente'
     select car_category.name, from: 'Categoria do carro'
     click_on 'Enviar'
 
-    expect(page).to have_content('Data de inicio: 09/12/2019')
-    expect(page).to have_content('Data de término: 12/12/2019')
+    expect(page).to have_content(1.day.from_now.strftime('%d/%m/%Y'))
+    expect(page).to have_content(2.days.from_now.strftime('%d/%m/%Y'))
     expect(page).to have_content('Cliente')
     expect(page).to have_content(client.name)
     expect(page).to have_content(client.document)
@@ -28,7 +28,7 @@ feature 'user schedule rental' do
     expect(page).to have_content(car_category.name)
   end
 
-  scenario 'and must fill all fields' do
+  scenario 'and must fill in all fields' do
     user = User.create!(email: 'test@gmail.com', password: '123456',
                         role: :employee)
     client = Client.create!(name: 'Fulano Sicrano', email: 'fulado@outlook.com',
@@ -55,13 +55,13 @@ feature 'user schedule rental' do
     login_as(user, scope: :user)
     visit rentals_path
     click_on 'Agendar locação'
-    fill_in 'Data de inicio', with: '12/12/2019'
-    fill_in 'Data de término', with: '09/12/2019'
+    fill_in 'Data de inicio', with: 2.days.from_now
+    fill_in 'Data de término', with: 1.day.from_now
     select "#{client.name} - #{client.document}", from: 'Cliente'
     select car_category.name, from: 'Categoria do carro'
     click_on 'Enviar'
 
-    expect(page).to have_content('Data de inicio deve ser menor que a data de término')
+    expect(page).to have_content('End date deve ser maior que data de ínicio')
   end
 
   scenario 'and must be logged in' do 
@@ -73,6 +73,6 @@ feature 'user schedule rental' do
   scenario 'and must be logged in to see the link to rentals' do
     visit root_path
 
-    expect(page).not_to have have_link('Locações')
+    expect(page).not_to have_link('Locações')
   end
 end
